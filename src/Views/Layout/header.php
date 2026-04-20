daa7637e8052abc45d2ce8320551cc8a04d0b642
Pablo Gualda <paguparra@gmail.com>
2026-04-20 14:22:59 +0200

    chore: sincronizar header con Github

--- src/Views/Layout/header.php
+++ src/Views/Layout/header.php
@@ -8,14 +8,13 @@
 </head>
 <body>
 <header>
-    <div id="logo">
-        <h1><a href="<?= BASE_URL ?>">Agenda</a> </h1>
-    </div>
+    
     <nav>
-        <ul>
-            <li><a href="<?=BASE_URL?>">Inicio</a></li>
-            <li><a href="<?= BASE_URL ?>Contacto/listar/">Listar contactos</a> </li>
-            <li><a href="<?= BASE_URL ?>Contacto/nuevoContacto/">Añadir nuevo contacto</a> </li>
-        </ul>
+        <div id="logo">
+            <img src="" alt="">
+        </div>
+        <div>
+            <input type="text">
+        </div>
     </nav>
 </header>
